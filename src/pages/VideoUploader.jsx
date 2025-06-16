c06df70272a3445a230823a86c3d30bbcd0b118a
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-06-16 21:20:46 +0530

    Added Files

--- src/pages/VideoUploader.jsx
+++ src/pages/VideoUploader.jsx
@@ -1,160 +1,147 @@
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import axios from 'axios';
-import { useDropzone } from 'react-dropzone';
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
 import { IoCloudUploadOutline } from "react-icons/io5";
-import { useSelector } from 'react-redux'
-import LoadingPage from './LoadingPage';
+import { useSelector } from "react-redux";
+import LoadingPage from "./LoadingPage";
 
 const VideoUpload = () => {
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+  const [videoFile, setVideoFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
+  const [compressedVideoUrl, setCompressedVideoUrl] = useState("");
 
+  const onDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    if (file && file.type.startsWith("video/")) {
+      setVideoFile(file);
+    } else {
+      alert("Please upload a valid video file.");
+    }
+  };
 
-    const navigate = useNavigate()
-    const { currentUser } = useSelector((state) => state.user)
-    console.log(currentUser)
-    const [videoFile, setVideoFile] = useState(null);
-    const [uploading, setUploading] = useState(false);
-    const [responseMessage, setResponseMessage] = useState("");
-    const [compressedVideoUrl, setCompressedVideoUrl] = useState("");
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: { "video/*": [] },
+  });
 
-    const onDrop = (acceptedFiles) => {
-        const file = acceptedFiles[0];
-        if (file && file.type.startsWith("video/")) {
-            setVideoFile(file);
-        } else {
-            alert("Please upload a valid video file.");
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop,
-        accept: { "video/*": [] },
-    });
-
-
-
-    const handleSubmit = async (e) => {
-
-        e.preventDefault()
-
-
-        if (!videoFile) {
-            alert('Please select a video to upload.');
-            return
-
-        }
-
-        setUploading(true)
-
-        const formData = new FormData()
-        formData.append('video', videoFile)
-
-        console.log("FormData", formData)
-
-        try {
-
-            console.log("Uploading video...")
-            const response = await axios.post(`https://vidtrim-backend.onrender.com/upload?id=${currentUser?._id}`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-                onUploadProgress: (progressEvent) => {
-                    const percentCompleted = Math.round(
-                        (progressEvent.loaded * 100) / progressEvent.total
-                    );
-                    console.log(`Uploading: ${percentCompleted}%`);
-                    setResponseMessage(`Uploading: ${percentCompleted}%`);
-                }
-            })
-
-
-
-
-            if (response.success === false) {
-                setUploading(false)
-                return;
-            }
-
-            console.log("moving",response)
-            const token = response.data.compressedVideoUrl
-            navigate(`/download-video?compressedVideoUrl=${encodeURIComponent(token)}`);
-
-
-
-
-        } catch (error) {
-
-            console.log(error.message)
-        }
-
-        finally {
-            setUploading(false)
-        }
+    if (!videoFile) {
+      alert("Please select a video to upload.");
+      return;
     }
 
+    setUploading(true);
 
+    const formData = new FormData();
+    formData.append("video", videoFile);
 
-    return (
-        uploading ? (
-            <LoadingPage />
-        ) : (
-            <section className="flex justify-center items-center pt-3 md:pt-6 w-full min-h-screen  ">
-                <div className=" w-[95%] md:w-[80%] border-[1px]   m-auto rounded-lg  bg-gray-100 flex flex-col text-center gap-5 p-5 ">
-                    <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-black font-sans">Compression Tool</h2>
+    console.log("FormData", formData);
 
-                    <div className="text-[#808080]  sm:text-lg  md:text-xl lg:text-2xl font-mono tracking-tighter">Quickly and easily compress large video files for smoother streaming,<br></br>
-                        faster downloads, and storage</div>
+    try {
+      // https://vidtrim-backend.onrender.com
+      console.log("Uploading video...");
+      const response = await axios.post(
+        `https://vidtrim-backend-vercel-1qz03qz42-chandrashekharsainis-projects.vercel.app/upload?id=${currentUser?._id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+        
+          },
+          timeout:10000,
+          onUploadProgress: (progressEvent) => {
+            const percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            console.log(`Uploading: ${percentCompleted}%`);
+            setResponseMessage(`Uploading: ${percentCompleted}%`);
+          },
+        }
+      );
 
+      if (response.success === false) {
+        setUploading(false);
+        return;
+      }
 
-                    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center ">
-                        <div
-                            {...getRootProps()}
-                            className="border-2 border-dashed border-blue-500 p-10 w-full sm:w-[90%] md:w-[80%] lg:w-[50%] flex flex-col justify-center items-center rounded-md  cursor-pointer"
-                        >
-                            <div className=" text-[65px] sm:text-[100px] text-gray-500 mt-3 sm:mt-5">
-                                <IoCloudUploadOutline />
-                            </div>
-                            <input {...getInputProps()} className="hidden " />
-                            <button
-                                type="button"
-                                className="mt-7 text-white px-8 sm:px-16 py-3 border-2 border-blue-500 bg-blue-500 rounded-full   font-semibold hover:bg-white hover:text-blue-500 transition duration-300"
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    document.querySelector('input[type="file"]').click();
-                                }}
-                            >
-                                Upload File
-                            </button>
-                            <p className="mt-9 sm:mt-8 md:mt-6 lg:mt-5 text-lg text-gray-500">or drop your file here</p>
-                        </div>
-                        <p className="mt-3 text-red-500">{responseMessage}</p>
-                        <button
-                            type="submit"
-                            disabled={uploading}
-                            className="bg-blue-500 text-white px-6 py-3 mt-5 rounded-full font-semibold hover:bg-blue-600 transition duration-300"
-                        >
-                            {uploading ? "Uploading..." : "Upload Video"}
-                        </button>
+      console.log("moving", response);
+      const token = response.data.compressedVideoUrl;
+      navigate(
+        `/download-video?compressedVideoUrl=${encodeURIComponent(token)}`
+      );
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setUploading(false);
+    }
+  };
 
-                        {/* {
+  return uploading ? (
+    <LoadingPage />
+  ) : (
+    <section className="flex justify-center items-center pt-3 md:pt-6 w-full min-h-screen  ">
+      <div className=" w-[95%] md:w-[80%] border-[1px]   m-auto rounded-lg  bg-gray-100 flex flex-col text-center gap-5 p-5 ">
+        <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-black font-sans">
+          Compression Tool
+        </h2>
+
+        <div className="text-[#808080]  sm:text-lg  md:text-xl lg:text-2xl font-mono tracking-tighter">
+          Quickly and easily compress large video files for smoother streaming,
+          <br></br>
+          faster downloads, and storage
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center items-center "
+        >
+          <div
+            {...getRootProps()}
+            className="border-2 border-dashed border-blue-500 p-10 w-full sm:w-[90%] md:w-[80%] lg:w-[50%] flex flex-col justify-center items-center rounded-md  cursor-pointer"
+          >
+            <div className=" text-[65px] sm:text-[100px] text-gray-500 mt-3 sm:mt-5">
+              <IoCloudUploadOutline />
+            </div>
+            <input {...getInputProps()} className="hidden " />
+            <button
+              type="button"
+              className="mt-7 text-white px-8 sm:px-16 py-3 border-2 border-blue-500 bg-blue-500 rounded-full   font-semibold hover:bg-white hover:text-blue-500 transition duration-300"
+              onClick={(e) => {
+                e.stopPropagation();
+                document.querySelector('input[type="file"]').click();
+              }}
+            >
+              Upload File
+            </button>
+            <p className="mt-9 sm:mt-8 md:mt-6 lg:mt-5 text-lg text-gray-500">
+              or drop your file here
+            </p>
+          </div>
+          <p className="mt-3 text-red-500">{responseMessage}</p>
+          <button
+            type="submit"
+            disabled={uploading}
+            className="bg-blue-500 text-white px-6 py-3 mt-5 rounded-full font-semibold hover:bg-blue-600 transition duration-300"
+          >
+            {uploading ? "Uploading..." : "Upload Video"}
+          </button>
+
+          {/* {
     responseMessage && (
         <p className="mt-5 text-red-500">{responseMessage}</p>
     )
 } */}
-
-
-                    </form>
-
-
-
-                </div >
-            </section >
-        )
-    )
-
-
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default VideoUpload;
-
-
