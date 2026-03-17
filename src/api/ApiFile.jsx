72061b4853f6b6cfda50fa80fe482cde0b796e27
Chandra Shekhar Saini <chandrashekharsaini32@gmail.com>
2026-03-17 15:53:34 +0530

    Edited Delete Api

--- src/api/ApiFile.jsx
+++ src/api/ApiFile.jsx
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-// Production
-// const serverUrl = axios.create({
-//   baseURL: "https://vidtrim-backend-vercel.vercel.app",
-//   headers: { "Content-Type": "application/json" },
-//   withCredentials: "true",
-// });
-
-//Local Development
+Production
 const serverUrl = axios.create({
-  baseURL: "http://localhost:3600",
+  baseURL: "https://vidtrim-backend-vercel.vercel.app",
   headers: { "Content-Type": "application/json" },
   withCredentials: "true",
 });
+
+//Local Development
+// const serverUrl = axios.create({
+//   baseURL: "http://localhost:3600",
+//   headers: { "Content-Type": "application/json" },
+//   withCredentials: "true",
+// });
 
 export default serverUrl;
